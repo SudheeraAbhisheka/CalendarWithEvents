133a3ee071edcdc39d7632ffbed862c901474164
Abhisheka <20535197@student.curtin.edu.au>
2024-05-23 15:52:45 +0530

    Calendar fragment

---
+++ app/src/main/java/com/example/calendar/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.calendar;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        CalendarFragment calendarFragment = (CalendarFragment) fm.findFragmentById(R.id.fragmentContainer_MainActivity);
+        calendarFragment = new CalendarFragment();
+        fm.beginTransaction().add(R.id.fragmentContainer_MainActivity, calendarFragment).commit();
+
+    }
+}
